e29a9e8fc22cfd6ca9135265ef261c3cf32c2e3c
Bartłomiej Zieliński <bartekz@gmail.com>
2021-09-10 23:11:33 +0200

    Init console test app1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+            Console.ReadKey();
+        }
+
+        static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
